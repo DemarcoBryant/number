148e00630b6d23e413a87fcadf038c3f65718234
DemarcoBryant <158539299+DemarcoBryant@users.noreply.github.com>
2024-02-12 13:25:17 -0500

    numbers

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int number = 17;
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
